35d6c0461b6925cdb816ff9c8a6c5eccacc782c8
jaszmije <jaszmije@gmail.com>
2024-02-10 12:50:50 +0100

    add calculate cost of ingredients

--- src/js/script.js
+++ src/js/script.js
@@ -58,7 +58,10 @@
             thisProduct.id = id;
             thisProduct.data = data;
             thisProduct.renderInMenu();
+            thisProduct.getElements();
             thisProduct.initAccordion();
+            thisProduct.initOrderForm();
+            thisProduct.processOrder();
             console.log('new Product', thisProduct);
         }
 
@@ -77,38 +80,116 @@
             menuContainer.appendChild(thisProduct.element);
         }
 
+        getElements() {
+            const thisProduct = this;
+
+            thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+            thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+            console.log('thisProduct.form', thisProduct.form)
+            thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+            console.log('thisProduct.formInputs', thisProduct.formInputs)
+            thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+            thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+        }
+
         initAccordion() {
             const thisProduct = this;
             console.log('thisProduct', thisProduct)
             /* find the clickable trigger (the element that should react to clicking) */
-            const clickableTriggers = thisProduct.element.querySelectorAll(select.menuProduct.clickable);
-            console.log('clickableTriggers', clickableTriggers);
-
             /* START: add event listener to clickable trigger on event click */
-            for (let clickableTrigger of clickableTriggers) {
-                clickableTrigger.addEventListener('click', function (event) {
-                    console.log('event', event);
+            thisProduct.accordionTrigger.addEventListener('click', function (event) {
+                console.log('event', event);
 
-                    // prevent default action for event
-                    event.preventDefault();
+                // prevent default action for event
+                event.preventDefault();
 
-                    // find active product (product that has active class)
-                    const activeProducts = thisProduct.element.querySelectorAll('.product__header.active');
-                    console.log('activeProducts', activeProducts);
+                // find active product (product that has active class)
+                const activeProducts = thisProduct.element.querySelectorAll('.product__header.active');
+                console.log('activeProducts', activeProducts);
 
-                    // if there is an active product and it's not thisProduct.element, remove class active from it
-                    for (let activeProduct of activeProducts) {
-                        if (activeProduct && activeProduct !== thisProduct.element) {
-                            activeProduct.classList.remove('active');
-                        }
+                // if there is an active product and it's not thisProduct.element, remove class active from it
+                for (let activeProduct of activeProducts) {
+                    if (activeProduct && activeProduct !== thisProduct.element) {
+                        activeProduct.classList.remove('active');
                     }
+                }
 
-                    // toggle active class on thisProduct.element
-                    thisProduct.element.classList.toggle('active');
+                // toggle active class on thisProduct.element
+                thisProduct.element.classList.toggle('active');
+            });
+        }
+
+        initOrderForm() {
+            const thisProduct = this;
+            console.log('initOrderForm was started');
+
+            thisProduct.form.addEventListener('submit', function (event) {
+                event.preventDefault();
+                thisProduct.processOrder();
+            });
+
+            for (let input of thisProduct.formInputs) {
+                input.addEventListener('change', function () {
+                    thisProduct.processOrder();
                 });
             }
+
+            thisProduct.cartButton.addEventListener('click', function (event) {
+                event.preventDefault();
+                thisProduct.processOrder();
+            });
         }
+
+        processOrder() {
+            const thisProduct = this;
+            console.log('processOrder was started');
+
+            const formData = utils.serializeFormToObject(thisProduct.form);
+            console.log('formData', formData);
+            // set price to default price
+            let price = thisProduct.data.price;
+
+            // for every category (param)...
+            for (let paramId in thisProduct.data.params) {
+                // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+                const param = thisProduct.data.params[paramId];
+                console.log(paramId, param);
+
+                // for every option in this category
+                for (let optionId in param.options) {
+                    // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+                    const option = param.options[optionId];
+                    console.log(optionId, option);
+
+                    // check if the option is selected in the form data
+                    const isOptionSelected = formData[paramId].includes(optionId);
+                    console.log(optionId, 'is selected?', isOptionSelected);
+
+                    // check if the option is not default and is selected
+                    const addCost = isOptionSelected && !option.default;
+                    if (addCost) {
+                        console.log('is selected ingredient additional ? ', addCost);
+
+                        // if yes add additional cost
+                        price += option.price;
+                    }
+                    const subtractCost = !isOptionSelected && option.default;
+                    // check if the option is default and not selected
+                    if (subtractCost) {
+                        console.log('is unselected ingredient default ?', subtractCost);
+                        // if yes, remove the cost of unselected basic ingredient
+                        price -= option.price;
+                    }
+                    console.log('price equals', price);
+                }
+            }
+
+            // update calculated price in the HTML
+            thisProduct.priceElem.innerHTML = price;
+        }
+
     }
+
 
     const
         app = {
